c0787ef46614e75057f6e67dc50c8fde3af5a09b
laksmanayudha <laksmanayudha22@gmail.com>
2025-04-30 00:16:26 +0800

    test: add exception test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,5 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: { ...globals.jest } } }
 ]);
